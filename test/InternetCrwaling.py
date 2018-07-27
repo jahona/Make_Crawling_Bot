03305b0d23a6fab8d71b87aec373a59f50c8bef9
jahona <jahona5240@gmail.com>
2018-07-27 22:42:36 +0900

    test file bugfix and line formatting

--- test/InternetCrwaling.py
+++ test/InternetCrwaling.py
@@ -12,21 +12,25 @@ random.seed(datetime.datetime.now())
 # 페이지에 발견된 내부 링크를 모두 목록으로 만듭니다.
 def getInternalLinks(bsObj, includeUrl):
 	internalLinks = []
+
 	# /로 시작하는 링크를 모두 찾습니다.
 	for link in bsObj.findAll("a", href=re.compile("^(/|.*"+includeUrl+")")):
 		if link.attrs['href'] is not None:
 			if link.attrs['href'] not in internalLinks:
 				internalLinks.append(link.attrs['href'])
-	return getInternalLinks
+
+	return internalLinks
 
 # 페이지에서 발견된 외부 링크를 모두 목록으로 만듭니다.
 def getExternalLinks(bsObj, excludeUrl):
 	externalLinks = []
+
 	# 현재 URL을 포함하지 않으면서 http나 www로 시작하는 링크를 모두 찾습니다.
 	for link in bsObj.findAll("a", href=re.compile("^(http|www)((?!"+excludeUrl+").)*$")):
 		if link.attrs['href'] is not None:
 			if link.attrs['href'] not in externalLinks:
 				externalLinks.append(link.attrs['href'])
+
 	return externalLinks
 
 def splitAddress(fullAddress):
@@ -43,8 +47,8 @@ def getRandomExternalLink(startingAddress):
 	externalLinks = getExternalLinks(bsObj, splitAddress(startingAddress)[0])
 
 	if len(externalLinks) == 0:
-		internalLinks = getInternalLinks(startingPage)
-		return getNextExternalLInk(internalLinks[random.randint(0, len(internalLinks)-1)])
+		internalLinks = getInternalLinks(startingAddress)
+		# return getNextExternalLInk(internalLinks[random.randint(0, len(internalLinks)-1)])
 	else:
 		return externalLinks[random.randint(0, len(externalLinks)-1)]
 
