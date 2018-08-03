684866ef68d4f69178692081e64dda7481ef7954
jahona <jahona5240@gmail.com>
2018-08-03 21:51:15 +0900

    removed escape character

--- test/n_gram.py
+++ test/n_gram.py
@@ -1,8 +1,15 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import ssl
+import re
 
 def ngrams(input, n):
+    input = re.sub('\n+', " ", input)
+    input = re.sub(' +', " ", input)
+    input = bytes(input, "UTF-8")
+    input = input.decode("ascii", "ignore")
+    print(input)
+
     input = input.split(' ')
     output = []
     for i in range(len(input)-n+1):
@@ -13,6 +20,6 @@ gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
 html = urlopen("http://en.wikipedia.org/wiki/Python_(programming_language)", context=gcontext)
 bsObj = BeautifulSoup(html, "html.parser")
 content = bsObj.find("div", {"id":"mw-content-text"}).get_text()
-ngrams = ngrams(content, 2)
-print(ngrams)
-print("2-grams count is: " + str(len(ngrams)))
+ngramsData = ngrams(content, 2)
+print(ngramsData)
+print("2-grams count is: " + str(len(ngramsData)))
